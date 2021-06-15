e853c2adea6f694bd2b923a4aa763aa401dc0fe2
kuroda kazumichi <kazumichi96@gmail.com>
2021-06-15 19:15:03 +0900

    Added debug

--- backend/app/Http/Controllers/Web/DebugController.php
+++ backend/app/Http/Controllers/Web/DebugController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Study;
 
 class DebugController extends Controller
 {
@@ -20,6 +21,17 @@ class DebugController extends Controller
    */
   public function plural(Request $request) {
     return Str::plural($request->get('word'));
+  }
+
+  public function query() 
+  {
+    DB::enableQueryLog();
+    $studies = Study::with(['category', 'variety'])->orderBy('order_no')->get();
+    foreach($studies as $study) {
+      echo $study->category->id;
+      echo $study->category->name;
+    }
+    dd(DB::getQueryLog());
   }
 
   public function debug01() {
