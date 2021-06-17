f3429732e651eec2590c1ca5afe7e999ed7773a3
kurodakazumichi <kazumichi96@gmail.com>
2021-06-17 09:06:10 +0900

    Laravelに定数ファイルを実装する #18

---
+++ backend/app/Consts/StudyProblemConsts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Consts;
+
+// Study Indexで使う定数
+class StudyProblemConsts 
+{
+  // 習得度(mastery)リストの定数
+  public const MASTERIES = [
+    0  => '未回答',
+    1  => 'Lv1',
+    2  => 'Lv2',
+    3  => 'Lv3',
+    4  => 'Lv4',
+    5  => 'Lv5',
+    6  => 'Lv6',
+    7  => 'Lv7',
+    8  => 'Lv8',
+    9  => 'Lv9',
+    10 => 'Master',
+  ];
+
+  public const KINDS = [
+    0 => '例題',
+    1 => 'EX',
+    2 => 'EXERCISES'
+    3 => '演習',
+    4 => '章末問題',
+  ]
+}
