5091f6e53ba0b83438c1fc87bab025d916423a8b
kurodakazumichi <kazumichi96@gmail.com>
2021-06-19 03:43:26 +0900

    ノート機能の実装 #13 下準備

---
+++ backend/app/Models/Note.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+  use HasFactory;
+  protected $fillable = ['category_id', 'variety_id', 'title', 'body'];
+  public static $rules = [
+    'category_id' => ['required', 'exists:categories,id'],
+    'variety_id'  => ['required', 'exists:varieties,id'],
+    'title'       => ['required', 'max:255'],
+    'body'        => ['required']
+  ];
+
+  public function category() {
+    return $this->belongsTo(Category::class);
+  }
+
+  public function variety() {
+    return $this->belongsTo(Variety::class);
+  }
+}
---
+++ backend/database/migrations/2021_06_19_033905_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+  /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+  public function up()
+  {
+    Schema::create('notes', function (Blueprint $table) {
+      $table->id();
+      $table->integer('category_id');
+      $table->integer('variety_id');
+      $table->string('title');
+      $table->text('body');
+      $table->timestamps();
+    });
+  }
+
+  /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+  public function down()
+  {
+    Schema::dropIfExists('notes');
+  }
+}
