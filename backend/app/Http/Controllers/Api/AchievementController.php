3652e7fe0b79fc0486b8b6514ba3576e5356cbf7
kurodakazumichi <kazumichi96@gmail.com>
2021-06-20 05:35:57 +0900

    Refactor

--- backend/app/Http/Controllers/Api/AchievementController.php
+++ backend/app/Http/Controllers/Api/AchievementController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Achievement;
+use Exception;
+
 class AchievementController extends Controller
 {
   /**
