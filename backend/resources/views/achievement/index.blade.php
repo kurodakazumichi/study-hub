92078d44c5331b7cd95e5f0b439f9e41ecc55626
kurodakazumichi <kazumichi96@gmail.com>
2021-06-25 23:04:54 +0900

    UI調整

--- backend/resources/views/achievement/index.blade.php
+++ backend/resources/views/achievement/index.blade.php
@@ -16,26 +16,35 @@
       {{---------------------------------------- 検索フォーム ----------------------------------------}}
       <div id="_tab-search">
         <form action="/studies" method="get">
-          <label for="search-category_id">カテゴリ</label>
-          <x-forms.drop-box 
-            id="search-category_id" 
-            name="category_id" 
-            hasEmpty="true" 
-            :options="$categories"
-            :selected="$search['category_id']"
-          />
-          
-          <label for="search-variety_id">バラエティ</label>
-          <x-forms.drop-box 
-            id="search-variety_id" 
-            name="variety_id" 
-            hasEmpty="true" 
-            :options="$varieties"
-            :selected="$search['variety_id']"
-          />
-
-          <input type="submit" value="検索">
-          <input type="button" value="リセット" onclick="location.href='/studies'; return false;" >
+          <table class="table">
+            <tbody>
+              <tr>
+                <th>カテゴリ</td>
+                <td>
+                  <x-forms.drop-box 
+                    name="category_id" 
+                    hasEmpty="true" 
+                    :options="$categories"
+                    :selected="$search['category_id']"
+                  />
+                </td>
+                <th>バラエティ</th>
+                <td>
+                  <x-forms.drop-box 
+                    id="search-variety_id" 
+                    name="variety_id" 
+                    hasEmpty="true" 
+                    :options="$varieties"
+                    :selected="$search['variety_id']"
+                  />
+                </td>
+                <td>
+                  <input class="btn" type="submit" value="検索">
+                  <input class="btn" type="button" value="リセット" onclick="location.href='/studies'; return false;" >
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </form>
       </div>
 
