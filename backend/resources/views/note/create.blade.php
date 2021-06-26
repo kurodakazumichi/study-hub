3f0676c41fe45c2f12c13d181904695a2655f968
kurodakazumichi <kazumichi96@gmail.com>
2021-06-26 15:54:23 +0900

    Note新規作成画面のスタイル調整 #42

--- backend/resources/views/note/create.blade.php
+++ backend/resources/views/note/create.blade.php
@@ -4,28 +4,45 @@
 @section('js', 'note/create.js')
 
 @section('main')
-  <section>
-    <h1>Study</h1>
+  
+    <h1 class="heading-lv1">Note</h1>
     <form id="create-form">
-      <label for="search-category_id">カテゴリ</label>
-      <x-forms.drop-box 
-        id="search-category_id" 
-        name="category_id" 
-        :options="$categories"
-      />
-        
-      <label for="search-variety_id">バラエティ</label>
-      <x-forms.drop-box 
-        id="search-variety_id" 
-        name="variety_id" 
-        :options="$varieties"
-      /><br>
 
-      タイトル：<input type="text" name="title" size="100"><br>
-
-      <textarea name="body" cols="100" rows="40"></textarea>
+      <table class="table w-100">
+        <tbody>
+          <tr>
+            <th style="width:10%;">カテゴリ</th>
+            <td style="width:40%;">
+              <x-forms.drop-box 
+                id="search-category_id" 
+                name="category_id" 
+                :options="$categories"
+              />
+            </td>
+            <th style="width:10%;">バラエティ</th>
+            <td style="width:40%;">
+              <x-forms.drop-box 
+                id="search-variety_id" 
+                name="variety_id" 
+                :options="$varieties"
+              />
+            </td>
+          </tr>
+          <tr>
+            <th>タイトル</th>
+            <td colspan="3">
+              <input type="text" name="title" class="w-100"><br>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="4" style="padding:0; border:none;">
+              <textarea class="w-100" name="body" rows="40"></textarea>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
       <input type="button" value="新規作成" id="create-button">
     </form>
-  </section>
+  
 @endsection
